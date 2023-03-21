7e33c8c103b9cf954ee7b744f38f529fd14f1b41
EslamAref75 <eslam.aref.777@gmail.com>
2023-03-21 10:08:58 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Fibonacci sequence is generated
+ * by adding the previous two terms.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+unsigned long fibo1 = 0, fibo2 = 1, sum;
+float total_sum;
+
+while (1)
+{
+sum = fibo1 + fibo2;
+if (sum > 4000000)
+break;
+
+if ((sum % 2) == 0)
+total_sum += sum;
+
+fibo1 = fibo2;
+fibo2 = sum;
+}
+printf("%.0f\n", total_sum);
+
+return (0);
+}
