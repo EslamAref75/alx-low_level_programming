64dd6dd02697dfa2428396ab5d868fe7cc6be405
EslamAref75 <eslam.aref.777@gmail.com>
2023-03-23 13:20:43 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,6 @@
  * for the multiples of five print Buzz
  * For numbers which are multiples of both three and
  * five print FizzBuzz.
- * 
  * Return: Always 0 (Success)
  */
 
@@ -17,7 +16,7 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 
-if (i % 3 == 0 )
+if (i % 3 == 0)
 {
 printf("Fizz");
 }
