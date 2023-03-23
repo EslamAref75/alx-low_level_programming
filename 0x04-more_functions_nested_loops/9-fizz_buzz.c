2ed82777c24ac2a87503c24b74a4890ca87c902c
EslamAref75 <eslam.aref.777@gmail.com>
2023-03-23 13:13:50 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,6 +7,7 @@
  * for the multiples of five print Buzz
  * For numbers which are multiples of both three and
  * five print FizzBuzz.
+ * 
  * Return: Always 0 (Success)
  */
 
@@ -19,17 +20,14 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf("Fizz");
-printf(" ");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf("Buzz");
-printf(" ");
 }
 else if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf("FizzBuzz");
-printf(" ");
 }
 else
 {
